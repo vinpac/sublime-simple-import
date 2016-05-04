bc4e5a390cf7e001fa96823a39ecd6368997c05d
Vinicius Pacheco <vin175pacheco@gmail.com>
2016-05-04 14:15:40 -0300

    Added example configuration

--- index.py
+++ index.py
@@ -148,7 +148,7 @@ class SimpleImport():
 
   @staticmethod
   def expressionInContext(expression, context):
-    match = re.search(r"[^\s]*{0}".format(expression), context)
+    match = re.search(r"[^\.;\s]*{0}".format(expression), context)
     if match:
       return match.group(0)
     return expression
